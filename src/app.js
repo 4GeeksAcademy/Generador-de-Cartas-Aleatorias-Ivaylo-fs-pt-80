6f7358f2f6d04b86fb1ab72eb4216057db832a85
Ivaylo Svetlinov Lilov <95366964+Ivaylo-Lilov@users.noreply.github.com>
2024-10-23 16:25:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const valores = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const palos = ["♦", "♥", "♠", "♣"];
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  const valorAleatorio = getRandomElement(valores);
+  const paloAleatorio = getRandomElement(palos);
+
+  const valorElemento = document.querySelector(".valor");
+  const paloSuperior = document.querySelector(".palo-superior");
+  const paloInferior = document.querySelector(".palo-inferior");
+
+  valorElemento.innerHTML = valorAleatorio;
+  paloSuperior.innerHTML = paloAleatorio;
+  paloInferior.innerHTML = paloAleatorio;
+
+  if (paloAleatorio === "♦" || paloAleatorio === "♥") {
+    paloSuperior.style.color = "red";
+    paloInferior.style.color = "red";
+    valorElemento.style.color = "red";
+  } else {
+    paloSuperior.style.color = "black";
+    paloInferior.style.color = "black";
+    valorElemento.style.color = "black";
+  }
 };
